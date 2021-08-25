2bcbef45e9448f1e97b3096ee57988100981adb3
Marlon Tereziano <73303855+azevgabriel@users.noreply.github.com>
2021-08-25 17:23:03 -0300

    Fix: Fix Warnings

--- src/App.tsx
+++ src/App.tsx
@@ -14,13 +14,13 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 16,
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//   },
+//   text: {
+//     fontSize: 16,
+//   },
+// });
